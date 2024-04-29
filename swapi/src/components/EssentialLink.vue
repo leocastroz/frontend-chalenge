9a7cad5178546a6590123484f6ba478ebcd7b68f
leocastroz <castroz.dev@gmail.com>
2024-04-29 14:30:42 -0300

    feat: improving entry style

--- swapi/src/components/EssentialLink.vue
+++ swapi/src/components/EssentialLink.vue
@@ -9,11 +9,11 @@
       v-if="props.icon"
       avatar
     >
-      <q-icon :name="props.icon" />
+      <q-icon :name="props.icon" class="tw-text-orange-300"/>
     </q-item-section>
 
     <q-item-section>
-      <q-item-label>{{ props.title }}</q-item-label>
+      <q-item-label class="tw-text-orange-400">{{ props.title }}</q-item-label>
       <q-item-label caption>{{ props.caption }}</q-item-label>
     </q-item-section>
   </q-item>
--- swapi/src/pages/IndexPage.vue
+++ swapi/src/pages/IndexPage.vue
@@ -1,10 +1,13 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="bg-image tw-flex tw-items-center tw-justify-center">
+    <div class="tw-grid tw-text-center">
+      <img src="../assets/images/logotype.png" alt="" width="170" class="tw-mx-auto">
+      <div class="tw-grid tw-text-center tw-items-center tw-justify-center tw-w-56">
+        <p class="tw-py-2 tw-text-xl tw-font-bold tw-text-black">Front-end challenge</p>
+        <p class=" tw-text-black tw-text-[18px]">consuming "swapi" for data demonstration</p>
+      </div>
+      <RouterLink to="/people" class="tw-py-2 tw-my-3 tw-rounded-full tw-bg-orange-800 tw-text-[18px]">see now</RouterLink>
+    </div>
   </q-page>
 </template>
 
