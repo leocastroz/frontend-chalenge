edc3ffbbd80cba583db292ccef11ae619a349ae3
leocastroz <castroz.dev@gmail.com>
2024-04-29 20:06:01 -0300

    refact: defaulting to the setup script

--- swapi/src/pages/GetPeoples.vue
+++ swapi/src/pages/GetPeoples.vue
@@ -29,123 +29,94 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import axios from "axios";
 import { ref } from "vue";
 
-export default {
-  setup() {
-    const columns = [
-      {
-        name: "name",
-        required: true,
-        label: "Name",
-        align: "left",
-        field: "name",
-        sortable: true,
-      },
-      {
-        name: "birth_year",
-        label: "Birth Year",
-        align: "center",
-        field: "birth_year",
-        sortable: true,
-      },
-      {
-        name: "eye_color",
-        label: "Eye Color",
-        align: "center",
-        field: "eye_color",
-        sortable: true,
-      },
-      {
-        name: "gender",
-        label: "Gender",
-        align: "center",
-        field: "gender",
-        sortable: true,
-      },
-      {
-        name: "height",
-        label: "Height",
-        align: "center",
-        field: "height",
-        sortable: true,
-      },
-      {
-        name: "hair_color",
-        label: "Hair Color",
-        align: "center",
-        field: "hair_color",
-        sortable: true,
-      },
-      // {
-      //   name: "mass",
-      //   label: "Mass",
-      //   align: "center",
-      //   field: "mass",
-      //   sortable: true,
-      // },
-      // {
-      //   name: "homeworld",
-      //   label: "Homeworld",
-      //   align: "center",
-      //   field: "homeworld",
-      //   sortable: true,
-      // },
-    ];
-
-    const pagination = ref({
-      rowsPerPage: 10,
-    });
-
-    const selectedPerson = ref(null);
-
-    const selectPerson = (evt, row) => {
-      selectedPerson.value = row;
-    };
-
-    const people = ref([]);
-    const nextPage = ref(null);
-    const previousPage = ref(null);
-
-    const fetchPeople = (url = "https://swapi.py4e.com/api/people/") => {
-      axios
-        .get(url)
-        .then((response) => {
-          people.value = response.data.results;
-          nextPage.value = response.data.next;
-          previousPage.value = response.data.previous;
-        })
-        .catch((error) => {
-          console.error("Error fetching people:", error);
-        });
-    };
-
-    const fetchNextPage = () => {
-      if (nextPage.value) {
-        fetchPeople(nextPage.value);
-      }
-    };
-
-    const fetchPreviousPage = () => {
-      if (previousPage.value) {
-        fetchPeople(previousPage.value);
-      }
-    };
-
-    fetchPeople();
-
-    return {
-      columns,
-      pagination,
-      people,
-      fetchNextPage,
-      fetchPreviousPage,
-      previousPage,
-      selectPerson,
-      selectedPerson
-    };
+const columns = [
+  {
+    name: "name",
+    required: true,
+    label: "Name",
+    align: "left",
+    field: "name",
+    sortable: true,
   },
+  {
+    name: "birth_year",
+    label: "Birth Year",
+    align: "center",
+    field: "birth_year",
+    sortable: true,
+  },
+  {
+    name: "eye_color",
+    label: "Eye Color",
+    align: "center",
+    field: "eye_color",
+    sortable: true,
+  },
+  {
+    name: "gender",
+    label: "Gender",
+    align: "center",
+    field: "gender",
+    sortable: true,
+  },
+  {
+    name: "height",
+    label: "Height",
+    align: "center",
+    field: "height",
+    sortable: true,
+  },
+  {
+    name: "hair_color",
+    label: "Hair Color",
+    align: "center",
+    field: "hair_color",
+    sortable: true,
+  },
+];
+
+const pagination = ref({
+  rowsPerPage: 10,
+});
+
+const selectedPerson = ref(null);
+
+const selectPerson = (evt, row) => {
+  selectedPerson.value = row;
 };
+
+const people = ref([]);
+const nextPage = ref(null);
+const previousPage = ref(null);
+
+const fetchPeople = (url = "https://swapi.py4e.com/api/people/") => {
+  axios
+    .get(url)
+    .then((response) => {
+      people.value = response.data.results;
+      nextPage.value = response.data.next;
+      previousPage.value = response.data.previous;
+    })
+    .catch((error) => {
+      console.error("Error fetching people:", error);
+    });
+};
+
+const fetchNextPage = () => {
+  if (nextPage.value) {
+    fetchPeople(nextPage.value);
+  }
+};
+
+const fetchPreviousPage = () => {
+  if (previousPage.value) {
+    fetchPeople(previousPage.value);
+  }
+};
+
+fetchPeople();
 </script>
